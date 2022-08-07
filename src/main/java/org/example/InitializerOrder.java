d986d590ec66bf947fbfc4177f440e4fbf3b691a
PauloPortofolio <pvba02@gmail.com>
2022-08-07 11:32:21 -0400

    1.0) Starting Project
    -> CONCLUDED:
    
    -> WORKING:
    
    -> NEXT STEPS:
    
    -> FUTURE IMPLEMENTATIONS:

---
+++ src/main/java/org/example/InitializerOrder.java
@@ -0,0 +1,39 @@
+package org.example;
+
+class OrderedClass {
+    static int statementOrder;
+
+    int b = clarify("assigning b");
+
+    {
+        clarify("initializer 2");
+    }
+    int a = clarify("assigning a");
+
+    {
+        clarify("initializer 1");
+    }
+
+    OrderedClass() {
+        clarify("constructor");
+    }
+
+    int c = clarify("assigning c");
+
+    {
+        clarify("initializer 3");
+    }
+
+    int clarify(String message) {
+        statementOrder++;
+        System.out.println(statementOrder + " " + message);
+        return statementOrder;
+    }
+}
+
+public class InitializerOrder {
+    public static void main(String[] args) {
+        // Instantiating the object should set off initialization code.
+        OrderedClass o = new OrderedClass();
+    }
+}
